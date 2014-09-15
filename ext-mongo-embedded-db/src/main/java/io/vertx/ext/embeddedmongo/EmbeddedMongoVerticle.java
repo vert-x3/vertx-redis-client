935f906639a8ec4389cf86688fff57af3d1c03e6
purplefox <timvolpe@gmail.com>
2014-09-15 11:37:35 +0100

    mongo embedded verticle

---
+++ ext-mongo-embedded-db/src/main/java/io/vertx/ext/embeddedmongo/EmbeddedMongoVerticle.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * The Apache License v2.0 is available at
+ * http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.ext.embeddedmongo;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * This verticle wraps an embedded MongoDB instance.
+ *
+ * It's useful if you have an application that uses MongoDB and you want to write unit tests for it
+ * that run without having to manually set-up a MongoDB.
+ *
+ * Just deploy the verticle before your test.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class EmbeddedMongoVerticle extends AbstractVerticle {
+
+  private MongodExecutable exe;
+
+  @Override
+  public void start() throws Exception {
+
+    if (vertx != null && !vertx.currentContext().isWorker()) {
+      throw new IllegalStateException("Must be started as worker verticle!");
+    }
+
+    int port = config.getInteger("port", 27017);
+
+    IMongodConfig config = new MongodConfigBuilder().
+      version(Version.Main.PRODUCTION).
+      net(new Net(port, Network.localhostIsIPv6())).
+      build();
+
+    exe = MongodStarter.getDefaultInstance().prepare(config);
+    exe.start();
+  }
+
+  @Override
+  public void stop() {
+    exe.stop();
+  }
+}
---
+++ ext-mongo-embedded-db/src/test/java/io/vertx/ext/embeddedmongo/test/EmbeddedMongoVerticleTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * The Apache License v2.0 is available at
+ * http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.ext.embeddedmongo.test;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.VertxOptions;
+import io.vertx.ext.embeddedmongo.EmbeddedMongoVerticle;
+import io.vertx.test.core.VertxTestBase;
+import org.junit.Test;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class EmbeddedMongoVerticleTest extends VertxTestBase {
+
+  @Override
+  public VertxOptions getOptions() {
+    // It can take some time to download the first time!
+    return VertxOptions.options().setMaxWorkerExecuteTime(30 * 60 * 1000);
+  }
+
+  @Test
+  public void testEmbeddedMongo() {
+    // Not really sure what to test here apart from start and stop
+    vertx.deployVerticle("java:" + EmbeddedMongoVerticle.class.getName(), DeploymentOptions.options().setWorker(true), onSuccess(deploymentID -> {
+      assertNotNull(deploymentID);
+      vertx.undeployVerticle(deploymentID, onSuccess(v -> {
+        assertNull(v);
+        testComplete();
+      }));
+    }));
+    await();
+  }
+}
