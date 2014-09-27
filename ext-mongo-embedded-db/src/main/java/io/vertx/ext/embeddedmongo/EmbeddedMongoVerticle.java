a40ea8b0895d7c5f778809e51b9acf86ffc507e8
nscavell <nick.scavelli@gmail.com>
2014-09-27 13:43:34 -0400

    Use a different port for embedded mongo so it doesn't conflict with default mongo port

--- ext-mongo-embedded-db/src/main/java/io/vertx/ext/embeddedmongo/EmbeddedMongoVerticle.java
+++ ext-mongo-embedded-db/src/main/java/io/vertx/ext/embeddedmongo/EmbeddedMongoVerticle.java
@@ -49,7 +49,7 @@ public class EmbeddedMongoVerticle extends AbstractVerticle {
 
     JsonObject config = vertx.context().config();
 
-    int port = config.getInteger("port", 27017);
+    int port = config.getInteger("port", 27018);
 
     IMongodConfig embeddedConfig = new MongodConfigBuilder().
       version(Version.Main.PRODUCTION).
