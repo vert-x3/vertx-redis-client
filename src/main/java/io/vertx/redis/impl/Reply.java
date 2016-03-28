0df3bcacec2639fd6a3b10c1a130b240f4a772bf
Ashwin Surana <surana.an@gmail.com>
2016-03-29 01:20:32 +0530

    fix for transactions.

--- src/main/java/io/vertx/redis/impl/Reply.java
+++ src/main/java/io/vertx/redis/impl/Reply.java
@@ -62,7 +62,7 @@ public final class Reply {
   @SuppressWarnings("unchecked")
   public <T> T asType(final Class<T> type, final String encoding) throws ClassCastException {
 
-    if (data == null) {
+    if (data == null || "QUEUED".equals(data)) {
       return null;
     }
 
