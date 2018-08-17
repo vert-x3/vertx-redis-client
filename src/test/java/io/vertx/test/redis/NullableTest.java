52acdea1dc6b39940cf79cf258c75b15f848bae2
okou19900722 <ilayan19900722@gmail.com>
2018-08-17 15:17:56 +0800

    Add nullable (#116)
    Async result operations should provide a nullable result

--- src/test/java/io/vertx/test/redis/NullableTest.java
+++ src/test/java/io/vertx/test/redis/NullableTest.java
@@ -1,44 +1,84 @@
 package io.vertx.test.redis;
 
+import io.vertx.core.AsyncResult;
 import org.junit.Test;
 
 public class NullableTest extends AbstractRedisClientBase {
 
   private static final String notExistsKey = "notExistsKey";
+
   @Test
   public void testGetNullable() {
-    redis.del(notExistsKey, r -> {
-      if (r.failed()) {
-        fail(r.cause());
-      } else {
-        redis.get(notExistsKey, asyncResult -> {
-          if (asyncResult.failed()) {
-            fail(asyncResult.cause());
-          } else {
-            assertNull(asyncResult.result());
-            testComplete();
-          }
-        });
-      }
+    makeSureKeyNotExists(() -> {
+      redis.get(notExistsKey, this::handle);
     });
     await();
   }
+
   @Test
   public void testGetBinaryNullable() {
+    makeSureKeyNotExists(() -> {
+      redis.getBinary(notExistsKey, asyncResult -> {
+        if (asyncResult.failed()) {
+          fail(asyncResult.cause());
+        } else {
+          assertNull(asyncResult.result());
+          testComplete();
+        }
+      });
+    });
+    await();
+  }
+
+  @Test
+  public void testGetSetNullable() {
+    makeSureKeyNotExists(() -> {
+      redis.getset(notExistsKey, "value", this::handle);
+    });
+    await();
+  }
+
+  @Test
+  public void testHGetNullable() {
+    makeSureKeyNotExists(() -> {
+      String notExistsField = "notExistsField";
+      redis.hget(notExistsKey, notExistsField, this::handle);
+    });
+    await();
+  }
+
+  @Test
+  public void testLPopNullable() {
+    makeSureKeyNotExists(() -> {
+      redis.lpop(notExistsKey, this::handle);
+    });
+    await();
+  }
+
+  @Test
+  public void testSPopNullable() {
+    makeSureKeyNotExists(() -> {
+      redis.spop(notExistsKey, this::handle);
+    });
+    await();
+  }
+
+  private void makeSureKeyNotExists(Runnable task) {
     redis.del(notExistsKey, r -> {
       if (r.failed()) {
         fail(r.cause());
       } else {
-        redis.getBinary(notExistsKey, asyncResult -> {
-          if (asyncResult.failed()) {
-            fail(asyncResult.cause());
-          } else {
-            assertNull(asyncResult.result());
-            testComplete();
-          }
-        });
+        task.run();
       }
     });
-    await();
+  }
+
+  private void handle(AsyncResult<String> asyncResult) {
+    if (asyncResult.failed()) {
+      fail(asyncResult.cause());
+    } else {
+      assertNull(asyncResult.result());
+      testComplete();
+    }
   }
 }
