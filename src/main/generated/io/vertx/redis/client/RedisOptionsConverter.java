a11c16604a2d3b1e49f4474092ee18ee1b33dd8a
Paulo Lopes <paulo@mlopes.net>
2018-11-23 16:42:24 +0100

    fixed codegen

---
+++ src/main/generated/io/vertx/redis/client/RedisOptionsConverter.java
@@ -0,0 +1,65 @@
+package io.vertx.redis.client;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converter for {@link io.vertx.redis.client.RedisOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.redis.client.RedisOptions} original class using Vert.x codegen.
+ */
+public class RedisOptionsConverter {
+
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, RedisOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "masterName":
+          if (member.getValue() instanceof String) {
+            obj.setMasterName((String)member.getValue());
+          }
+          break;
+        case "maxWaitingHandlers":
+          if (member.getValue() instanceof Number) {
+            obj.setMaxWaitingHandlers(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "netClientOptions":
+          if (member.getValue() instanceof JsonObject) {
+            obj.setNetClientOptions(new io.vertx.core.net.NetClientOptions((JsonObject)member.getValue()));
+          }
+          break;
+        case "role":
+          if (member.getValue() instanceof String) {
+            obj.setRole(io.vertx.redis.RedisRole.valueOf((String)member.getValue()));
+          }
+          break;
+        case "useSlave":
+          if (member.getValue() instanceof String) {
+            obj.setUseSlave(io.vertx.redis.RedisSlaves.valueOf((String)member.getValue()));
+          }
+          break;
+      }
+    }
+  }
+
+  public static void toJson(RedisOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(RedisOptions obj, java.util.Map<String, Object> json) {
+    if (obj.getMasterName() != null) {
+      json.put("masterName", obj.getMasterName());
+    }
+    json.put("maxWaitingHandlers", obj.getMaxWaitingHandlers());
+    if (obj.getNetClientOptions() != null) {
+      json.put("netClientOptions", obj.getNetClientOptions().toJson());
+    }
+    if (obj.getRole() != null) {
+      json.put("role", obj.getRole().name());
+    }
+    if (obj.getUseSlave() != null) {
+      json.put("useSlave", obj.getUseSlave().name());
+    }
+  }
+}
