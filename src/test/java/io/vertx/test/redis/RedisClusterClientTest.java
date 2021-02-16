596beea9d6bbfaf67838b6352654d18f09387979
Paulo Lopes <pmlopes@gmail.com>
2021-02-16 11:48:08 +0100

    Avoid vertx-unit
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/test/java/io/vertx/test/redis/RedisClusterClientTest.java
+++ src/test/java/io/vertx/test/redis/RedisClusterClientTest.java
@@ -6,7 +6,6 @@ import io.vertx.redis.client.RedisClientType;
 import io.vertx.redis.client.RedisOptions;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -14,9 +13,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.Wait;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.assertTrue;
@@ -28,28 +25,23 @@ public class RedisClusterClientTest {
   @Parameterized.Parameters
   public static Iterable<String[]> data() {
     return Arrays.asList(
-      new String[] {"redis:5", "abcd"},
-      new String[] {"redis:5", null},
-      new String[] {"redis:6", "abcd"},
-      new String[] {"redis:6", null});
+      new String[]{"redis:5", "abcd"},
+      new String[]{"redis:5", null},
+      new String[]{"redis:6", "abcd"},
+      new String[]{"redis:6", null});
   }
 
   public final Network network = Network.newNetwork();
 
-  @Rule
   public final GenericContainer<?> redis7000;
-  @Rule
   public final GenericContainer<?> redis7001;
-  @Rule
   public final GenericContainer<?> redis7002;
-  @Rule
   public final GenericContainer<?> redis7003;
-  @Rule
   public final GenericContainer<?> redis7004;
-  @Rule
   public final GenericContainer<?> redis7005;
 
   public GenericContainer<?> redisCli;
+
   private final String password;
 
   public RedisClusterClientTest(String image, String password) {
@@ -90,10 +82,32 @@ public class RedisClusterClientTest {
       .withNetwork(network)
       .withCommand(cmd);
 
+    StringBuilder stringBuilder = new StringBuilder();
+    for (GenericContainer<?> container : Arrays.asList(redis7000, redis7001, redis7002, redis7003, redis7004, redis7005)) {
+      container
+        .start();
+
+      container
+        .getContainerInfo()
+        .getNetworkSettings()
+        .getNetworks()
+        .values()
+        .stream()
+        .findFirst()
+        .ifPresent(net -> stringBuilder.append(net.getIpAddress()).append(":").append("6379").append(" "));
+    }
+
+    String connectionString = stringBuilder.substring(0, stringBuilder.length() - 1);
+    String cmdline = String.format("redis-cli --cluster create %s --cluster-replicas 1 --cluster-yes%s", connectionString, password == null ? "" : " -a " + password);
+    System.out.println(cmdline);
+
     redisCli = new GenericContainer<>(image)
       .withNetwork(network).waitingFor(
         Wait.forLogMessage(".*OK.*", 2)
-      );
+      )
+      .withCommand(cmdline);
+
+    redisCli.start();
   }
 
   private Vertx vertx;
@@ -101,70 +115,47 @@ public class RedisClusterClientTest {
 
   @Before
   public void before() {
-      List<GenericContainer<?>> containerList = new ArrayList<>();
-      containerList.add(redis7000);
-      containerList.add(redis7001);
-      containerList.add(redis7002);
-      containerList.add(redis7003);
-      containerList.add(redis7004);
-      containerList.add(redis7005);
-      StringBuilder stringBuilder = new StringBuilder();
-      for (GenericContainer<?> container : containerList) {
-        container
-          .getContainerInfo()
-          .getNetworkSettings()
-          .getNetworks()
-          .values()
-          .stream()
-          .findFirst()
-          .ifPresent(containerNetwork ->
-            stringBuilder.append(containerNetwork.getIpAddress()).append(":").append("6379").append(" "));
-      }
-
-      String connectionString = stringBuilder.substring(0, stringBuilder.length() - 1);
-      String cmd = String.format("redis-cli --cluster create %s --cluster-replicas 1 --cluster-yes%s", connectionString, password == null ? "" : " -a " + password);
-      System.out.println(cmd);
-      redisCli.setCommand(cmd);
-      redisCli.start();
-
-      vertx = Vertx.vertx();
-      client = Redis.createClient(
-        vertx, new RedisOptions()
-          .setType(RedisClientType.CLUSTER)
-          .addConnectionString(
-            password != null ?
-              "redis://:" + password + "@" + redis7000.getContainerIpAddress() + ":" + redis7000.getFirstMappedPort() :
-              "redis://" + redis7000.getContainerIpAddress() + ":" + redis7000.getFirstMappedPort())
-      );
+    vertx = Vertx.vertx();
+    client = Redis.createClient(
+      vertx, new RedisOptions()
+        .setType(RedisClientType.CLUSTER)
+        .addConnectionString(
+          password != null ?
+            "redis://:" + password + "@" + redis7000.getContainerIpAddress() + ":" + redis7000.getFirstMappedPort() :
+            "redis://" + redis7000.getContainerIpAddress() + ":" + redis7000.getFirstMappedPort()));
   }
 
   @After
   public void after() {
     try {
       client.close();
-    } catch (RuntimeException e) {}
-    try {
-      redisCli.close();
-    } catch (RuntimeException e) {}
+    } catch (RuntimeException e) {
+    }
+    for (GenericContainer<?> container : Arrays.asList(redis7000, redis7001, redis7002, redis7003, redis7004, redis7005, redisCli)) {
+      try {
+        container.close();
+      } catch (RuntimeException e) {
+      }
+    }
     try {
       vertx.close();
-    } catch (RuntimeException e) {}
+    } catch (RuntimeException e) {
+    }
   }
 
   @Test(timeout = 10_000L)
   public void testConnect() throws InterruptedException {
-    CountDownLatch test = new CountDownLatch(1);
-    vertx.runOnContext(v -> {
-      client.connect(onConnect -> {
-        if (onConnect.failed()) {
-          fail(onConnect.cause().getMessage());
-        } else {
-          assertTrue(onConnect.succeeded());
-          test.countDown();
-        }
-      });
+    final CountDownLatch latch = new CountDownLatch(1);
+
+    client.connect(onConnect -> {
+      if (onConnect.failed()) {
+        fail(onConnect.cause().getMessage());
+      } else {
+        assertTrue(onConnect.succeeded());
+        latch.countDown();
+      }
     });
 
-    test.await();
+    latch.await();
   }
 }
