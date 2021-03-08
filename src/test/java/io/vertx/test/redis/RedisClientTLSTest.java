1b709e1f767c9e4b407b88c0c253253f4b2da19a
Paulo Lopes <pmlopes@gmail.com>
2021-03-08 09:32:38 +0100

    don't lock the port
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/test/java/io/vertx/test/redis/RedisClientTLSTest.java
+++ src/test/java/io/vertx/test/redis/RedisClientTLSTest.java
@@ -2,6 +2,7 @@ package io.vertx.test.redis;
 
 import io.vertx.core.net.JksOptions;
 import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.streams.Pump;
 import io.vertx.ext.unit.Async;
@@ -12,10 +13,7 @@ import io.vertx.redis.client.Command;
 import io.vertx.redis.client.Redis;
 import io.vertx.redis.client.RedisOptions;
 import io.vertx.redis.client.Request;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.testcontainers.containers.GenericContainer;
 
@@ -26,11 +24,15 @@ public class RedisClientTLSTest {
   public final RunTestOnContext rule = new RunTestOnContext();
 
   @ClassRule
-  public static final GenericContainer<?> redis = new GenericContainer<>("redis:6")
+  public static final GenericContainer<?> redis = new GenericContainer<>("redis:6.2.1")
     .withExposedPorts(6379);
 
+  private volatile NetServer  server;
+
   @Before
-  public void test(TestContext should) {
+  public void setUp(TestContext should) {
+    final Async test = should.async();
+
     // redis server (OSS) does not include TLS out of the box
     // users are expected to serve it behind a TLS tunnel
     // this test fakes it by creating a tunnel to the final open redis server
@@ -54,7 +56,24 @@ public class RedisClientTLSTest {
             Pump.pump(sockA, sockB).start();
             Pump.pump(sockB, sockA).start();
           });
-      }).listen(1234);
+      }).listen(0)
+      .onFailure(should::fail)
+      .onSuccess(server -> {
+        this.server = server;
+        test.complete();
+      });
+  }
+
+  @After
+  public void tearDown(TestContext should) {
+    final Async test = should.async();
+
+    server.close()
+      .onFailure(should::fail)
+      .onSuccess(v -> {
+        server = null;
+        test.complete();
+      });
   }
 
   @Test
@@ -66,7 +85,7 @@ public class RedisClientTLSTest {
       new RedisOptions()
         // were using self signed certificates so we need to trust all
         .setNetClientOptions(new NetClientOptions().setTrustAll(true))
-        .setConnectionString("rediss://localhost:1234"));
+        .setConnectionString("rediss://localhost:" + server.actualPort()));
 
     client.connect()
       .onFailure(should::fail)
@@ -89,7 +108,7 @@ public class RedisClientTLSTest {
       new RedisOptions()
         // were using self signed certificates so we need to trust all
         .setNetClientOptions(new NetClientOptions().setSsl(true).setTrustAll(true))
-        .setConnectionString("rediss://localhost:1234"));
+        .setConnectionString("rediss://localhost:" + server.actualPort()));
 
     client.connect()
       .onFailure(should::fail)
@@ -113,7 +132,7 @@ public class RedisClientTLSTest {
         // were using self signed certificates so we need to trust all
         .setNetClientOptions(new NetClientOptions().setSsl(true).setTrustAll(true))
         // pool is SSL but connection string isn't
-        .setConnectionString("redis://localhost:1234"));
+        .setConnectionString("redis://localhost:" + server.actualPort()));
 
     client.connect()
       .onFailure(t -> test.complete())
