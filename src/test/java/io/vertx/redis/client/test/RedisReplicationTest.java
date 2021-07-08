72b5903339e9fc4d30a145b141234bfef6ddb4e1
Paulo Lopes <pmlopes@gmail.com>
2021-07-08 21:35:40 +0200

    Detect the expected port during test
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/test/java/io/vertx/redis/client/test/RedisReplicationTest.java
+++ src/test/java/io/vertx/redis/client/test/RedisReplicationTest.java
@@ -9,6 +9,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.nio.charset.StandardCharsets;
+
 @RunWith(VertxUnitRunner.class)
 public class RedisReplicationTest {
 
@@ -33,7 +35,6 @@ public class RedisReplicationTest {
         onCreate.result()
           .send(Request.cmd(Command.INFO), info -> {
             should.assertTrue(info.succeeded());
-            System.out.println(info.result().toString());
             should.assertTrue(info.result().toString().contains("tcp_port:7000"));
             test.complete();
           });
@@ -44,25 +45,43 @@ public class RedisReplicationTest {
   public void testGetClientToReplica(TestContext should) {
     final Async test = should.async();
 
-    Redis.createClient(
-      rule.vertx(),
-      new RedisOptions()
-        .setType(RedisClientType.REPLICATION)
-        .setUseReplicas(RedisReplicas.NEVER)
-        .addConnectionString("redis://localhost:7004")
-        .setMaxPoolSize(4)
-        .setMaxPoolWaiting(16))
-      .connect(onCreate -> {
-        // get a connection to the master node
-        should.assertTrue(onCreate.succeeded());
-        // query the info
-        onCreate.result()
-          .send(Request.cmd(Command.INFO), info -> {
-            should.assertTrue(info.succeeded());
-            // even though we list the replica node, the main connection happens to the master
-            should.assertTrue(info.result().toString().contains("tcp_port:7000"));
-            test.complete();
-          });
+    Redis.createClient(rule.vertx(), "redis://localhost:7000")
+      .send(Request.cmd(Command.INFO).arg("REPLICATION"))
+      .onFailure(should::fail)
+      .onSuccess(res -> {
+        String[] lines = res.toString(StandardCharsets.ISO_8859_1).split("\r\n");
+        for (String line : lines) {
+          if (line.startsWith("slave0")) {
+            String[] parts = line.split(",");
+            for (String part : parts) {
+              if (part.startsWith("port=")) {
+                String port = part.substring(5);
+
+                // real start of the test
+                Redis.createClient(
+                  rule.vertx(),
+                  new RedisOptions()
+                    .setType(RedisClientType.REPLICATION)
+                    .setUseReplicas(RedisReplicas.NEVER)
+                    .addConnectionString("redis://localhost:" + port)
+                    .setMaxPoolSize(4)
+                    .setMaxPoolWaiting(16))
+                  .connect(onCreate -> {
+                    // get a connection to the master node
+                    should.assertTrue(onCreate.succeeded());
+                    // query the info
+                    onCreate.result()
+                      .send(Request.cmd(Command.INFO), info -> {
+                        should.assertTrue(info.succeeded());
+                        // even though we list the replica node, the main connection happens to the master
+                        should.assertTrue(info.result().toString().contains("tcp_port:7000"));
+                        test.complete();
+                      });
+                  });
+              }
+            }
+          }
+        }
       });
   }
 
@@ -70,25 +89,43 @@ public class RedisReplicationTest {
   public void testGetClientToReplicaUseReplicasAlways(TestContext should) {
     final Async test = should.async();
 
-    Redis.createClient(
-      rule.vertx(),
-      new RedisOptions()
-        .setType(RedisClientType.REPLICATION)
-        .setUseReplicas(RedisReplicas.ALWAYS)
-        .addConnectionString("redis://localhost:7004")
-        .setMaxPoolSize(4)
-        .setMaxPoolWaiting(16))
-      .connect(onCreate -> {
-        // get a connection to the master node
-        should.assertTrue(onCreate.succeeded());
-        // query the info
-        onCreate.result()
-          .send(Request.cmd(Command.INFO), info -> {
-            should.assertTrue(info.succeeded());
-            // we force read commands to go to replicas
-            should.assertTrue(info.result().toString().contains("tcp_port:7004"));
-            test.complete();
-          });
+    Redis.createClient(rule.vertx(), "redis://localhost:7000")
+      .send(Request.cmd(Command.INFO).arg("REPLICATION"))
+      .onFailure(should::fail)
+      .onSuccess(res -> {
+        String[] lines = res.toString(StandardCharsets.ISO_8859_1).split("\r\n");
+        for (String line : lines) {
+          if (line.startsWith("slave0")) {
+            String[] parts = line.split(",");
+            for (String part : parts) {
+              if (part.startsWith("port=")) {
+                String port = part.substring(5);
+
+                // real start of the test
+                Redis.createClient(
+                  rule.vertx(),
+                  new RedisOptions()
+                    .setType(RedisClientType.REPLICATION)
+                    .setUseReplicas(RedisReplicas.ALWAYS)
+                    .addConnectionString("redis://localhost:" + port)
+                    .setMaxPoolSize(4)
+                    .setMaxPoolWaiting(16))
+                  .connect(onCreate -> {
+                    // get a connection to the master node
+                    should.assertTrue(onCreate.succeeded());
+                    // query the info
+                    onCreate.result()
+                      .send(Request.cmd(Command.INFO), info -> {
+                        should.assertTrue(info.succeeded());
+                        // we force read commands to go to replicas
+                        should.assertTrue(info.result().toString().contains("tcp_port:" + port));
+                        test.complete();
+                      });
+                  });
+              }
+            }
+          }
+        }
       });
   }
 }
