6538dab3bb4e64b3fd3e7471acbba528657c04e8
Pablo Gutierrez <gutmox@gmail.com>
2021-11-22 18:39:39 +0100

    Adding simple test to check user passed is used and not default

--- src/test/java/io/vertx/redis/impl/RedisClientImplTest.java
+++ src/test/java/io/vertx/redis/impl/RedisClientImplTest.java
@@ -1,12 +1,28 @@
 package io.vertx.redis.impl;
 
+import io.vertx.core.Context;
 import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.redis.RedisOptions;
+import io.vertx.redis.client.Redis;
+import io.vertx.redis.client.RedisAPI;
 import io.vertx.redis.client.impl.RedisURI;
+import java.util.UUID;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(VertxUnitRunner.class)
 public class RedisClientImplTest {
+
+  @Rule
+  public RunTestOnContext rule = new RunTestOnContext();
+
+  private Redis client;
 
   @Test
   @SuppressWarnings("deprecation")
@@ -19,5 +35,18 @@ public class RedisClientImplTest {
     RedisURI redisURI = new RedisURI(client.getConnectionString());
     Assert.assertEquals("Password is not correct", auth, redisURI.password());
     vertx.close();
+  }
+
+  @Test
+  public void testUsernameIsNotDefault(TestContext testCxt) {
+
+    final Async before = testCxt.async();
+
+    client = Redis.createClient(rule.vertx(), new io.vertx.redis.client.RedisOptions().setConnectionString("redis://USER_NOT_DEFAULT:pwd@localhost:7002"));
+    client.connect(onConnect -> {
+      testCxt.assertTrue(onConnect.failed());
+      testCxt.assertTrue(onConnect.cause().toString().contains("WRONGPASS invalid username-password"));
+      before.complete();
+    });
   }
 }
