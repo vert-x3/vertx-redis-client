23d516f8f8ba415b5fa30078c1c0bb7b6c868239
Julien Viet <julien@julienviet.com>
2023-12-07 13:48:04 +0100

    Fix incorrect package for JsonGen annotation

--- src/main/java/io/vertx/redis/client/RedisOptions.java
+++ src/main/java/io/vertx/redis/client/RedisOptions.java
@@ -17,7 +17,7 @@ package io.vertx.redis.client;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.annotations.GenIgnore;
-import io.vertx.codegen.annotations.JsonGen;
+import io.vertx.codegen.json.annotations.JsonGen;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.tracing.TracingPolicy;
