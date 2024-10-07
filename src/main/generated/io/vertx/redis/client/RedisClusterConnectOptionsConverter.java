8e173ea9d497c9e3713b93f5ec8f8b2930802cda
Julien Viet <julien@julienviet.com>
2024-10-07 23:42:30 +0200

    Regen converters

--- src/main/generated/io/vertx/redis/client/RedisClusterConnectOptionsConverter.java
+++ src/main/generated/io/vertx/redis/client/RedisClusterConnectOptionsConverter.java
@@ -4,16 +4,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.redis.client.RedisClusterConnectOptions}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.redis.client.RedisClusterConnectOptions} original class using Vert.x codegen.
  */
 public class RedisClusterConnectOptionsConverter {
-
-  private static final Base64.Decoder BASE64_DECODER = Base64.getUrlDecoder();
-  private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, RedisClusterConnectOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
