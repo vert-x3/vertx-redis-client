1d8f94a2b5bcae23134d0aece96d6dde7f23d94a
Dmytro Buryak <dmytro.buryak@gmail.com>
2025-11-30 12:14:23 +0200

    Binary keys support in MultiType

--- src/main/java/io/vertx/redis/client/impl/types/MultiType.java
+++ src/main/java/io/vertx/redis/client/impl/types/MultiType.java
@@ -16,56 +16,54 @@
 package io.vertx.redis.client.impl.types;
 
 import io.vertx.codegen.annotations.Nullable;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.redis.client.Response;
 import io.vertx.redis.client.ResponseType;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 
 /**
  * A Redis MULTI response can represent a List/Set/Map type.
+ * <p>
+ * Not thread safe.
  */
 public final class MultiType implements Multi {
+  // shorthand aliases for better readability
+  private static final Function<Response, String> AS_STRING = Response::toString;
+  private static final Function<Response, Buffer> AS_BUFFER = Response::toBuffer;
 
   public static final MultiType EMPTY_MULTI = new MultiType(0, false);
   public static final MultiType EMPTY_MAP = new MultiType(0, true);
 
-  public static MultiType create(long length, boolean asMap) {
-    if (asMap) {
-      return new MultiType((int) length * 2, true);
-    } else {
-      return new MultiType((int) length, false);
-    }
+  public static MultiType create(long length, boolean isMap) {
+    return new MultiType((int) length, isMap);
   }
 
-  // only one of these will be not null
-  private final Map<String, Response> map;
+  // elements of this Multi are stored in this array
   private final Response[] multi;
-  // the expected size
-  private final int size;
+  private final boolean isMap;
+  // these maps are built on demand from the `multi` array
+  private Map<String, Response> strMap;
+  private Map<Buffer, Response> binMap;
   // mutable temporary state
-  private int count;
-  private String key;
+  private int count = 0;
 
-  private MultiType(int size, boolean asMap) {
-    if (asMap) {
-      this.multi = null;
-      this.map = new HashMap<>(size, 1.0f);
-    } else {
-      this.multi = new Response[size];
-      this.map = null;
-    }
-    this.size = size;
-    this.count = 0;
+  private MultiType(int size, boolean isMap) {
+    this.isMap = isMap;
+    // if `isMap`, then the `size` is the number of key-value pairs,
+    // so we need to allocate double size for the array
+    this.multi = new Response[isMap ? size * 2 : size];
   }
 
-  private MultiType(String key, Response value) {
-    this.map = null;
-    this.multi = new Response[]{SimpleStringType.create(key), value};
-    this.size = 2;
+  private MultiType(Response key, Response value) {
+    this.isMap = true;
+    this.multi = new Response[]{key, value};
   }
 
   @Override
@@ -75,142 +73,108 @@ public final class MultiType implements Multi {
 
   @Override
   public void add(Response reply) {
-    // if this Multi was created as a Map
-    if (map != null) {
-      if (count % 2 == 0) {
-        if (reply == null || reply.type() == null) {
-          throw new IllegalArgumentException("Map key is NULL or untyped");
-        }
-
-        switch (reply.type()) {
-          case BULK:
-          case SIMPLE:
-            key = reply.toString();
-            break;
-          default:
-            throw new IllegalArgumentException("Map key is not BULK or SIMPLE");
-        }
-      } else {
-        if (key != null) {
-          map.put(key, reply);
-        }
-        // clear the key
-        key = null;
-      }
-    }
-    // if this Multi was created as a Collection
-    if (multi != null) {
-      this.multi[this.count] = reply;
-    }
-    // increment the counter
+    this.multi[this.count] = reply;
     count++;
   }
 
   public boolean complete() {
-    return count == size;
+    return count == multi.length;
   }
 
   @Override
   public Response get(int index) {
-    if (multi != null) {
-      return multi[index];
+    if (isMap) {
+      throw new RuntimeException("Multi is a Map");
     }
-    throw new RuntimeException("Multi is a Map");
+    return multi[index];
   }
 
   @Override
-  public @Nullable Response get(String key) {
-    if (map != null) {
-      return map.get(key);
+  public Response get(String key) {
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.get(key);
     }
 
-    if (multi != null) {
-      // fallback (emulate old behavior)
-      if (multi.length % 2 == 0) {
-        // if the size is even we assume we can handle it as Map
-        for (int i = 0; i < multi.length; i += 2) {
-          if (key.equals(multi[i].toString())) {
-            return multi[i + 1];
-          }
-        }
-        // not found
-        return null;
-      }
-    }
-
-    throw new RuntimeException("Number of key is not even can't handle as Map");
+    // fallback (emulate old behavior)
+    return findInMultiArray(key, AS_STRING);
   }
 
   @Override
   public boolean containsKey(String key) {
-    if (map != null) {
-      return map.containsKey(key);
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.containsKey(key);
     }
 
-    if (multi != null) {
-      // fallback (emulate old behavior)
-      if (multi.length % 2 == 0) {
-        // if the size is even we assume we can handle it as Map
-        for (int i = 0; i < multi.length; i += 2) {
-          if (key.equals(multi[i].toString())) {
-            return true;
-          }
-        }
-        // not found
-        return false;
-      }
-    }
-
-    throw new RuntimeException("Number of key is not even can't handle as Map");
+    // fallback (emulate old behavior)
+    return findInMultiArray(key, AS_STRING) != null;
   }
 
   @Override
   public Set<String> getKeys() {
-    if (map != null) {
-      return map.keySet();
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.keySet();
     }
 
-    if (multi != null) {
-      // fallback (emulate old behavior)
-      if (multi.length % 2 == 0) {
-        final Set<String> keys = new HashSet<>();
-        // if the size is even we assume we can handle it as Map
-        for (int i = 0; i < multi.length; i += 2) {
-          switch (multi[i].type()) {
-            case BULK:
-            case SIMPLE:
-              keys.add(multi[i].toString());
-              break;
-          }
-        }
+    // fallback (emulate old behavior)
+    return getKeysOfMultiArray(AS_STRING);
+  }
 
-        return keys;
-      }
+  @Override
+  public Response get(Buffer key) {
+    if (isMap) {
+      buildBinMapIfNeeded();
+      return binMap.get(key);
     }
 
-    throw new RuntimeException("Number of key is not even can't handle as Map");
+    // fallback (emulate old behavior)
+    return findInMultiArray(key, AS_BUFFER);
+  }
+
+  @Override
+  public boolean containsKey(Buffer key) {
+    if (isMap) {
+      buildBinMapIfNeeded();
+      return binMap.containsKey(key);
+    }
+
+    // fallback (emulate old behavior)
+    return findInMultiArray(key, AS_BUFFER) != null;
+  }
+
+  @Override
+  public Set<Buffer> getBinaryKeys() {
+    if (isMap) {
+      buildBinMapIfNeeded();
+      return binMap.keySet();
+    }
+
+    // fallback (emulate old behavior)
+    return getKeysOfMultiArray(AS_BUFFER);
   }
 
   @Override
   public int size() {
-    return size;
+    return multi.length;
   }
 
   @Override
   public boolean isArray() {
-    return multi != null;
+    return !isMap;
   }
 
   @Override
   public boolean isMap() {
-    return map != null;
+    return isMap;
   }
 
   @Override
   public String toString() {
     final StringBuilder sb = new StringBuilder();
 
-    if (multi != null) {
+    if (!isMap) {
       sb.append('[');
       boolean more = false;
       for (Response r : multi) {
@@ -226,22 +190,15 @@ public final class MultiType implements Multi {
         more = true;
       }
       sb.append(']');
-    }
-
-    if (map != null) {
-      sb.append('{');
-      boolean more = false;
-      for (Map.Entry<String, Response> kv : map.entrySet()) {
-        if (more) {
-          sb.append(", ");
-        }
-
-        sb.append(kv.getKey());
-        sb.append(": ");
-        sb.append(kv.getValue());
-        more = true;
+    } else {
+      // if binMap is built, then the caller interprets keys as binary
+      if (binMap != null) {
+        // encode buffers in base64 ready for usage in JSON
+        mapToString(sb, binMap, Buffer::toJson);
+      } else {
+        buildStrMapIfNeeded();
+        mapToString(sb, strMap, Function.identity());
       }
-      sb.append('}');
     }
 
     return sb.toString();
@@ -249,40 +206,101 @@ public final class MultiType implements Multi {
 
   @Override
   public Iterator<Response> iterator() {
-    if (multi != null) {
-      return new Iterator<Response>() {
-        private int idx = 0;
+    return new Iterator<>() {
+      private int idx = 0;
 
-        @Override
-        public boolean hasNext() {
-          return idx < size;
-        }
+      @Override
+      public boolean hasNext() {
+        return idx < multi.length;
+      }
 
-        @Override
-        public Response next() {
+      @Override
+      public Response next() {
+        if (isMap) {
+          // wrap the key/value pair into a single multi response
+          return new MultiType(multi[idx++], multi[idx++]);
+        } else {
           return multi[idx++];
         }
-      };
+      }
+    };
+  }
+
+  private void assertCanInterpretMultiAsMap() {
+    // if the size is even, we assume we can handle it as Map
+    if (multi.length % 2 != 0) {
+      throw new RuntimeException("Number of elements is not even, can't handle as Map");
     }
+  }
 
-    if (map != null) {
-      return new Iterator<Response>() {
-        private final Iterator<Map.Entry<String, Response>> it = map.entrySet().iterator();
+  private <K> Set<K> getKeysOfMultiArray(Function<Response, K> converter) {
+    assertCanInterpretMultiAsMap();
 
-        @Override
-        public boolean hasNext() {
-          return it.hasNext();
-        }
-
-        @Override
-        public Response next() {
-          // wrap the kv into a single multi response
-          final Map.Entry<String, Response> kv = it.next();
-          return new MultiType(kv.getKey(), kv.getValue());
-        }
-      };
+    Set<K> convertedKeys = new LinkedHashSet<>();
+    // if the size is even, we assume we can handle it as Map
+    for (int i = 0; i < multi.length; i += 2) {
+      switch (multi[i].type()) {
+        case BULK:
+        case SIMPLE:
+          convertedKeys.add(converter.apply(multi[i]));
+          break;
+      }
     }
+    return convertedKeys;
+  }
 
-    throw new UnsupportedOperationException("Cannot iterator over NULL");
+  private <K> Response findInMultiArray(K key, Function<Response, K> converter) {
+    assertCanInterpretMultiAsMap();
+
+    for (int i = 0; i < multi.length; i += 2) {
+      K respInKeyFormat = converter.apply(multi[i]);
+      if (key.equals(respInKeyFormat)) {
+        return multi[i + 1];
+      }
+    }
+    // not found
+    return null;
+  }
+
+  private void buildStrMapIfNeeded() {
+    if (!isMap || strMap != null) {
+      return;
+    }
+    // redis tracks the order of entries in hashes, for some use-cases it may
+    // be important to preserve it, so we use LinkedHashMap
+    strMap = new LinkedHashMap<>(multi.length / 2);
+    for (int i = 0; i < multi.length; i += 2) {
+      // Response.toString() triggers the conversion of the response to UTF-8 String
+      strMap.put(multi[i].toString(), multi[i + 1]);
+    }
+  }
+
+  private void buildBinMapIfNeeded() {
+    if (!isMap || binMap != null) {
+      return;
+    }
+    // redis tracks the order of entries in hashes, for some use-cases it may
+    // be important to preserve it, so we use LinkedHashMap
+    binMap = new LinkedHashMap<>(multi.length / 2);
+    for (int i = 0; i < multi.length; i += 2) {
+      // Response.toBuffer() triggers the conversion of the response to Buffer
+      binMap.put(multi[i].toBuffer(), multi[i + 1]);
+    }
+  }
+
+  private static <T> void mapToString(StringBuilder sb, Map<T, Response> map, Function<T, String> keyToString) {
+    sb.append('{');
+    boolean more = false;
+    for (Map.Entry<T, Response> kv : map.entrySet()) {
+      if (more) {
+        sb.append(", ");
+      }
+
+      sb.append(keyToString.apply(kv.getKey()));
+      sb.append(": ");
+      sb.append(kv.getValue());
+      more = true;
+    }
+    sb.append('}');
   }
 }
