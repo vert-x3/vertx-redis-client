b8c2e7e993b9562b73f4783fa2c15c4ec81f2c0f
Dmytro Buryak <dmytro.buryak@gmail.com>
2025-12-18 01:27:49 +0200

    MultiType redesign to evaluate bin or/and str map lazily

--- src/main/java/io/vertx/redis/client/impl/types/MultiType.java
+++ src/main/java/io/vertx/redis/client/impl/types/MultiType.java
@@ -20,16 +20,18 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.redis.client.Response;
 import io.vertx.redis.client.ResponseType;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 
 /**
  * A Redis MULTI response can represent a List/Set/Map type.
+ * <p>
+ * Not thread safe.
  */
 public final class MultiType implements Multi {
   // shorthand aliases for better readability
@@ -40,41 +42,28 @@ public final class MultiType implements Multi {
   public static final MultiType EMPTY_MAP = new MultiType(0, true);
 
   public static MultiType create(long length, boolean asMap) {
-    if (asMap) {
-      return new MultiType((int) length * 2, true);
-    } else {
-      return new MultiType((int) length, false);
-    }
+    return new MultiType((int) length, asMap);
   }
 
-  // only one of these will be not null
-  private final Map<StrKey, Response> strMap;
+  // elements of this Multi are stored in "multi" array, and corresponding
+  // maps are built on demand
   private final Response[] multi;
-  private Map<BinKey, Response> binMap; // evaluated when binary keys are used
-  // the expected size
-  private final int size;
+  private final boolean isMap;
+  private Map<String, Response> strMap;
+  private Map<Buffer, Response> binMap;
   // mutable temporary state
-  private int count;
-  private StrKey key;
+  private int count = 0;
 
   private MultiType(int size, boolean asMap) {
-    if (asMap) {
-      this.multi = null;
-      // redis tracks the order of entries in hashes, for some use-cases it may
-      // be important to preserve it, so we use LinkedHashMap
-      this.strMap = new LinkedHashMap<>(size, 1.0f);
-    } else {
-      this.multi = new Response[size];
-      this.strMap = null;
-    }
-    this.size = size;
-    this.count = 0;
+    this.isMap = asMap;
+    // if asMap==true, then the "size" is the number of key-value pairs,
+    // so we need to allocate double size for the array
+    this.multi = new Response[asMap ? size * 2 : size];
   }
 
-  private MultiType(WrappedResp wrappedKey, Response value) {
-    this.strMap = null;
-    this.multi = new Response[]{wrappedKey.rawResp(), value};
-    this.size = 2;
+  private MultiType(Response key, Response value) {
+    this.isMap = true;
+    this.multi = new Response[]{key, value};
   }
 
   @Override
@@ -84,53 +73,27 @@ public final class MultiType implements Multi {
 
   @Override
   public void add(Response reply) {
-    // if this Multi was created as a Map
-    if (strMap != null) {
-      if (count % 2 == 0) {
-        if (reply == null || reply.type() == null) {
-          throw new IllegalArgumentException("Map key is NULL or untyped");
-        }
-
-        switch (reply.type()) {
-          case BULK:
-          case SIMPLE:
-            key = new StrKey(reply);
-            break;
-          default:
-            throw new IllegalArgumentException("Map key is not BULK or SIMPLE");
-        }
-      } else {
-        if (key != null) {
-          strMap.put(key, reply);
-        }
-        // clear the key
-        key = null;
-      }
-    }
-    // if this Multi was created as a Collection
-    if (multi != null) {
-      this.multi[this.count] = reply;
-    }
-    // increment the counter
+    this.multi[this.count] = reply;
     count++;
   }
 
   public boolean complete() {
-    return count == size;
+    return count == multi.length;
   }
 
   @Override
   public Response get(int index) {
-    if (multi != null) {
-      return multi[index];
+    if (isMap) {
+      throw new RuntimeException("Multi is a Map");
     }
-    throw new RuntimeException("Multi is a Map");
+    return multi[index];
   }
 
   @Override
   public @Nullable Response get(String key) {
-    if (strMap != null) {
-      return strMap.get(new StrKey(key));
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.get(key);
     }
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
@@ -141,8 +104,9 @@ public final class MultiType implements Multi {
 
   @Override
   public boolean containsKey(String key) {
-    if (strMap != null) {
-      return strMap.containsKey(new StrKey(key));
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.containsKey(key);
     }
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
@@ -153,91 +117,76 @@ public final class MultiType implements Multi {
 
   @Override
   public Set<String> getKeys() {
-    if (strMap != null) {
-      Set<String> strKeys = new LinkedHashSet<>();
-      for (StrKey k : strMap.keySet()) {
-        strKeys.add(k.asStr());
-      }
-      return strKeys;
+    if (isMap) {
+      buildStrMapIfNeeded();
+      return strMap.keySet();
     }
-
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
       return getKeysOfMultiArray(AS_STRING);
     }
-
     throw new RuntimeException("Number of key is not even can't handle as Map");
   }
 
   @Override
   public Response get(Buffer binKey) {
-    if (strMap != null) {
+    if (isMap) {
       buildBinMapIfNeeded();
-      return binMap.get(new BinKey(binKey));
+      return binMap.get(binKey);
     }
-
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
       return findInMultiArray(binKey, AS_BIN);
     }
-
     throw new RuntimeException("Number of key is not even can't handle as Map");
   }
 
   @Override
   public boolean containsKey(Buffer binKey) {
-    if (strMap != null) {
+    if (isMap) {
       buildBinMapIfNeeded();
-      return binMap.containsKey(new BinKey(binKey));
+      return binMap.containsKey(binKey);
     }
-
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
       return findInMultiArray(binKey, AS_BIN) != null;
     }
-
     throw new RuntimeException("Number of key is not even can't handle as Map");
   }
 
   @Override
   public Set<Buffer> getBinaryKeys() {
-    if (strMap != null) {
+    if (isMap) {
       buildBinMapIfNeeded();
-      Set<Buffer> binKeys = new LinkedHashSet<>();
-      for (BinKey k : binMap.keySet()) {
-        binKeys.add(k.asBin());
-      }
-      return binKeys;
+      return binMap.keySet();
     }
-
     if (canInterpretMultiArrayAsMap()) {
       // fallback (emulate old behavior)
       return getKeysOfMultiArray(AS_BIN);
     }
-
     throw new RuntimeException("Number of key is not even can't handle as Map");
   }
 
   @Override
   public int size() {
-    return size;
+    return multi.length;
   }
 
   @Override
   public boolean isArray() {
-    return multi != null;
+    return !isMap;
   }
 
   @Override
   public boolean isMap() {
-    return strMap != null;
+    return isMap;
   }
 
   @Override
   public String toString() {
     final StringBuilder sb = new StringBuilder();
 
-    if (multi != null) {
+    if (!isMap) {
       sb.append('[');
       boolean more = false;
       for (Response r : multi) {
@@ -253,25 +202,15 @@ public final class MultiType implements Multi {
         more = true;
       }
       sb.append(']');
-    }
-
-    if (strMap != null) {
-      sb.append('{');
-      boolean more = false;
+    } else {
       // if binMap is built, then the caller interprets keys as binary
-      Map<? extends WrappedResp, Response> mapToUse =
-        Objects.requireNonNullElse(binMap, strMap);
-      for (Map.Entry<? extends WrappedResp, Response> kv : mapToUse.entrySet()) {
-        if (more) {
-          sb.append(", ");
-        }
-
-        sb.append(kv.getKey());
-        sb.append(": ");
-        sb.append(kv.getValue());
-        more = true;
+      if (binMap != null) {
+        // encode buffers in base64 ready for usage in JSON
+        mapToString(sb, binMap, Buffer::toJson);
+      } else {
+        buildStrMapIfNeeded();
+        mapToString(sb, strMap, Function.identity());
       }
-      sb.append('}');
     }
 
     return sb.toString();
@@ -279,13 +218,13 @@ public final class MultiType implements Multi {
 
   @Override
   public Iterator<Response> iterator() {
-    if (multi != null) {
+    if (!isMap) {
       return new Iterator<Response>() {
         private int idx = 0;
 
         @Override
         public boolean hasNext() {
-          return idx < size;
+          return idx < multi.length;
         }
 
         @Override
@@ -293,11 +232,9 @@ public final class MultiType implements Multi {
           return multi[idx++];
         }
       };
-    }
-
-    if (strMap != null) {
+    } else {
       return new Iterator<Response>() {
-        private final Iterator<Map.Entry<StrKey, Response>> it = strMap.entrySet().iterator();
+        private final Iterator<Response> it = Arrays.stream(multi).iterator();
 
         @Override
         public boolean hasNext() {
@@ -307,23 +244,20 @@ public final class MultiType implements Multi {
         @Override
         public Response next() {
           // wrap the kv into a single multi response
-          final Map.Entry<StrKey, Response> kv = it.next();
-          return new MultiType(kv.getKey(), kv.getValue());
+          return new MultiType(it.next(), it.next());
         }
       };
     }
-
-    throw new UnsupportedOperationException("Cannot iterator over NULL");
   }
 
   private boolean canInterpretMultiArrayAsMap() {
-    // if the size is even we assume we can handle it as Map
-    return multi != null && multi.length % 2 == 0;
+    // if the size is even, we assume we can handle it as Map
+    return multi.length % 2 == 0;
   }
 
   private <K> Set<K> getKeysOfMultiArray(Function<Response, K> converter) {
-    Set<K> convertedKeys = new LinkedHashSet<K>();
-    // if the size is even we assume we can handle it as Map
+    Set<K> convertedKeys = new LinkedHashSet<>();
+    // if the size is even, we assume we can handle it as Map
     for (int i = 0; i < multi.length; i += 2) {
       switch (multi[i].type()) {
         case BULK:
@@ -346,127 +280,47 @@ public final class MultiType implements Multi {
     return null;
   }
 
-  private void buildBinMapIfNeeded() {
-    if (binMap != null) {
+  private void buildStrMapIfNeeded() {
+    if (!isMap || strMap != null) {
       return;
     }
-    binMap = new LinkedHashMap<>(strMap.size(), 1.0f);
-    for (Map.Entry<StrKey, Response> e : strMap.entrySet()) {
-      binMap.put(new BinKey(e.getKey().rawResp()), e.getValue());
+    // redis tracks the order of entries in hashes, for some use-cases it may
+    // be important to preserve it, so we use LinkedHashMap
+    strMap = new LinkedHashMap<>(multi.length / 2, 1.0f);
+    for (int i = 0; i < multi.length; i += 2) {
+      // Response.toString() triggers the conversion of the response to
+      // UTF-8 String
+      strMap.put(multi[i].toString(), multi[i + 1]);
     }
   }
 
-  private abstract static class WrappedResp {
-    private final Response rawResp;
-
-    WrappedResp(Response rawResp) {
-      this.rawResp = rawResp;
+  private void buildBinMapIfNeeded() {
+    if (!isMap || binMap != null) {
+      return;
     }
-
-    Response rawResp() {
-      return rawResp;
-    }
-  }
-
-  /**
-   * Wrapper over {@link Response} that allows using it as a Map key,
-   * and allows comparison (equals/hashCode) with both other {@link StrKey}
-   * instances and with {@link String} directly. Needed to support lookups
-   * by String keys provided by callers.
-   */
-  private static class StrKey extends WrappedResp {
-    private final String asStr;
-
-    StrKey(Response rawResp) {
-      super(rawResp);
-      this.asStr = rawResp.toString();
-    }
-
-    StrKey(String strKey) {
-      super(null);
-      this.asStr = strKey;
-    }
-
-    String asStr() {
-      return asStr;
-    }
-
-    @Override
-    public int hashCode() {
-      return asStr.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (o == this) {
-        return true;
-      }
-      if (o == null) {
-        return false;
-      }
-      if (o.getClass() == String.class) {
-        return asStr.equals(o);
-      } else if (o.getClass() == StrKey.class) {
-        return asStr.equals(((StrKey) o).asStr);
-      } else {
-        return false;
-      }
-    }
-
-    @Override
-    public String toString() {
-      return asStr;
+    // redis tracks the order of entries in hashes, for some use-cases it may
+    // be important to preserve it, so we use LinkedHashMap
+    binMap = new LinkedHashMap<>(multi.length / 2, 1.0f);
+    for (int i = 0; i < multi.length; i += 2) {
+      // Response.toBuffer() triggers the conversion of the response to Buffer
+      binMap.put(multi[i].toBuffer(), multi[i + 1]);
     }
   }
 
-  /**
-   * Wrapper over {@link Response} that allows using it as a Map key,
-   * and allows comparison (equals/hashCode) with both other {@link BinKey}
-   * instances and with {@link Buffer} directly. Needed to support lookups
-   * by binary keys ({@link Buffer}) provided by callers.
-   */
-  private static class BinKey extends WrappedResp {
-    private final Buffer asBin;
-
-    BinKey(Response rawResp) {
-      super(rawResp);
-      this.asBin = rawResp.toBuffer();
-    }
-
-    BinKey(Buffer binKey) {
-      super(null);
-      this.asBin = binKey;
-    }
-
-    Buffer asBin() {
-      return asBin;
-    }
-
-    @Override
-    public int hashCode() {
-      return asBin.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (o == this) {
-        return true;
+  private <T> void mapToString(StringBuilder sb, Map<T, Response> mapToUse,
+    Function<T, String> keyToString) {
+    sb.append('{');
+    boolean more = false;
+    for (Map.Entry<T, Response> kv : mapToUse.entrySet()) {
+      if (more) {
+        sb.append(", ");
       }
-      if (o == null) {
-        return false;
-      }
-      if (o instanceof Buffer) {
-        return asBin.equals(o);
-      } else if (o.getClass() == BinKey.class) {
-        return asBin.equals(((BinKey) o).asBin);
-      } else {
-        return false;
-      }
-    }
 
-    @Override
-    public String toString() {
-      return asBin.toJson(); // to base64 in JSON standard
+      sb.append(keyToString.apply(kv.getKey()));
+      sb.append(": ");
+      sb.append(kv.getValue());
+      more = true;
     }
+    sb.append('}');
   }
 }
